03b6cef9efc04ac9f9169465ab9048c50778209e
Md Aref Billah <mdarefbillah818@gmail.com>
2021-09-30 05:18:04 +0600

    os demo done

---
+++ reference/os_demo.js
@@ -0,0 +1,28 @@
+const os = require('os');
+
+
+//Platform
+
+console.log(os.platform())
+
+
+//CPU Arc
+
+console.log(os.arch())
+
+
+//CPU core info
+
+console.log(os.cpus())
+
+//Free memory
+console.log(os.freemem())
+
+//Total mem
+console.log(os.totalmem())
+
+//Home directory
+console.log(os.homedir())
+
+//uptime
+console.log(os.uptime())
